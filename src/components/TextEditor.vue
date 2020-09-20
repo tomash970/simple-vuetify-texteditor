3344f5d29dc8dcb51318ab47aa24838ed8f9eb9c
tomash970 <tpiljek@gmail.com>
2020-09-20 11:39:47 +0200

    first commit

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-card dark max-width="768px">
+      <div >
+        <v-btn class="font-weight-bold" tile>B</v-btn>
+        <v-btn class="font-italic" tile>I</v-btn>
+        <v-btn  class="text-decoration-underline" tile>U</v-btn>
+        <v-btn  class="text-decoration-line-through" tile>T</v-btn>
+        <v-btn><v-icon>mdi-format-list-bulleted</v-icon></v-btn>
+        <v-btn><v-icon>mdi-format-list-numbered</v-icon></v-btn>
+        <v-btn tile>P</v-btn>
+        <v-btn tile>H1</v-btn>
+        <v-btn tile>H2</v-btn>
+        <v-btn tile>H3</v-btn>
+        <v-btn tile>H4</v-btn>
+        <v-btn tile>B</v-btn>
+      </div>
+      <v-card-text >
+        <v-textarea
+          
+          v-html="text"
+        ></v-textarea>
+      </v-card-text>
+    </v-card>
+    
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'TextEditor',
+
+    data: () => ({
+      text: "<h1>Paragraph changed!</h1><p>To je to!</p>"
+    }),
+    methods: {
+      
+    }
+    
+  }
+</script>
---
+++ tests/unit/TextEditor.spec.js
@@ -0,0 +1,8 @@
+//import { shallowMount } from '@vue/test-utils'
+//import TextEditor from '@/components/TextEditor.vue'
+
+describe('TextEditor.vue', () => {
+  it('is sane', () => {
+    return
+  })
+})
